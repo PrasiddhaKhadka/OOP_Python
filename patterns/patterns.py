adc29bff990d087914401937ebc3f6f2afb8e4dd
PrasiddhaKhadka <prasiddha@thefireflytech.com>
2025-02-04 07:58:26 +0545

    Add: loops

---
+++ patterns/patterns.py
@@ -0,0 +1,17 @@
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+
+for i in range(1, 6):
+    print(" * " * i)
+
+
+for i in range(1, 7):
+    print(" * " * (6-i))
+    
+    
+for i in range (1,8):
+    print(" " * (7-i) + " * " * i)
